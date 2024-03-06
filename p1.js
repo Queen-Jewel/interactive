790705526cf39b76a0da286be806414e61bbb3ca
Jeweljeff <ketachi.jeff@gmial.com>
2024-03-06 21:35:21 +0100

    toggle

--- p1.js
+++ p1.js
@@ -1,24 +1,23 @@
-const rates = document.querySelectorAll('.span');
-const button = document.querySelector('.btn');
+const rates = document.querySelectorAll(".span");
+const button = document.querySelector(".btn");
 
-button.addEventListener('click', (e)=> {
-    
-
-    
-
-    let url = './index2.html';
-    window.location.href = url
+button.addEventListener("click", (e) => {
+  let url = "./index2.html";
+  window.location.href = url;
 });
 
 rates.forEach((rate) => {
-    rate.addEventListener('click', ()=> {
-        const rateValue = rate.innerText
+  rate.addEventListener("click", () => {
+    const rateValue = rate.innerText;
 
-        rate.style.background = 'Orange';
-        rate.style.color = 'White';
-        rate.innerText = rateValue;
-        window.sessionStorage.setItem('rateValue', rateValue)
-    })
+    if (!rate.dataset.clicked) {
+      rate.style.background = "Orange";
+      rate.style.color = "White";
+      rate.innerText = rateValue;
+      window.sessionStorage.setItem("rateValue", rateValue);
+    } else {
+      rate.removeAttribute("data-clicked");
+      rate.removeAttribute("style");
+    }
+  });
 });
-
-
